1d7bb74e479dd0e5fcbd490323ba7762349b8785
Isabella Tang <Isabellatang3425@gmail.com>
2025-11-28 09:37:19 -0500

    nov 28

--- Personalize-SAM-main/persam_cup.py
+++ Personalize-SAM-main/persam_cup.py
@@ -298,7 +298,7 @@ def persam_f(args, obj_name, images_path, masks_path, referenceImageName, output
     print('======> Mask weights:\n', weights_np)
 
     accuracies = {}
-    for alpha in [0.5]:
+    for alpha in [0]:
         print('======> Start Testing')
         correct = 0
         test_images = os.listdir(test_images_path)
